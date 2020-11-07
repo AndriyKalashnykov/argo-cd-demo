6f7d2be5a9e2831a3e4a80f9bf4c35eee8592d1c
Andriy Kalashnykov <AndriyKalashnykov@gmail.com>
2020-11-06 19:35:55 -0500

    added pwd

--- demo-app/build-push.sh
+++ demo-app/build-push.sh
@@ -7,6 +7,6 @@ docker build . -t spring-petclinic
 
 docker tag spring-petclinic $DOCKER_LOGIN/spring-petclinic:latest
 
-docker login -u $DOCKER_LOGIN
+docker login -u $DOCKER_LOGIN -p $DOCKER_PWD
 
 docker push $DOCKER_LOGIN/spring-petclinic
