3efd06a61de45327c7ed3e86acb6b245fb292bb3
Andriy Kalashnykov <AndriyKalashnykov@gmail.com>
2020-12-07 20:42:26 -0500

    added tag parameter

--- demo-app/build-push.sh
+++ demo-app/build-push.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
+TAG=${1:-latest}
 
 DOCKER_LOGIN=andriykalashnykov
 
 docker build . -t spring-petclinic
 
-docker tag spring-petclinic $DOCKER_LOGIN/spring-petclinic:latest
+docker tag spring-petclinic $DOCKER_LOGIN/spring-petclinic:$TAG
 
 docker login -u $DOCKER_LOGIN -p $DOCKER_PWD
 
