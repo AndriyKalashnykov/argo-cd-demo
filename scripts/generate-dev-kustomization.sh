5d64fd9f665f885f4077d1d44011912d6ef6c9b8
Andriy Kalashnykov <AndriyKalashnykov@gmail.com>
2020-12-09 21:07:23 -0500

    updated kustomization with IMAGE_TAG=1.3

---
+++ scripts/generate-dev-kustomization.sh
@@ -0,0 +1,26 @@
+
+#!/bin/bash
+
+set -x
+
+LAUNCH_DIR=$(pwd)
+SCRIPT_DIR=$(dirname $0)
+SCRIPT_PARENT_DIR="$(dirname "$SCRIPT_DIR")"
+SCRIPT_PARENT_DIR_FULL=$(pwd)
+
+cd $SCRIPT_DIR
+
+. ./set-env.sh
+
+cd $SCRIPT_PARENT_DIR_FULL/demo-app/kustomize/overlays/dev
+
+pwd
+
+IMAGE_TAG=1.3
+
+envsubst < kustomization.template.yaml > kustomization.yaml
+
+git commit -am "updated kustomization with IMAGE_TAG=$IMAGE_TAG"
+git push
+
+cd $LAUNCH_DIR
