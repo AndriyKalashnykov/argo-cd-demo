3def7aa7e374c0903a303cb4f4b480ccc1a3ed2c
Andriy Kalashnykov <AndriyKalashnykov@gmail.com>
2020-12-11 14:41:44 -0500

    updated ArgoCD installation

--- scripts/uninstall-argo-cd.sh
+++ scripts/uninstall-argo-cd.sh
@@ -10,6 +10,6 @@ cd $SCRIPT_PARENT_DIR
 
 kubectl delete -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/$ARGOCD_VER/manifests/install.yaml
 # kubectl delete -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
-kubectl -n argocd get all
+kubectl -n $NS_NAME get all
 
 cd $LAUNCH_DIR
